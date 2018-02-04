e8c1ad375a71956c7c11b58394a7ef58afdd4c9d
Neha Chaudhary <neha.chaudhary@hotmail.com>
2018-02-03 18:43:26 -0800

    Add default value to reducer

--- src/reducers/select-level-reducer.js
+++ src/reducers/select-level-reducer.js
@@ -2,7 +2,6 @@
 export default function (state = null, action) {
   switch (action.type) {
     case 'SELECTED_LEVEL':
-    console.log('action is', action);
       return action.payload;
     default:
       return 0;
