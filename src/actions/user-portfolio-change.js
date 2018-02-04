69568930a6d0f34fc1da6ca4ff26910e8a5d566d
Neha Chaudhary <neha.chaudhary@hotmail.com>
2018-02-04 07:50:44 -0800

    Write action for user portfolio update

---
+++ src/actions/user-portfolio-change.js
@@ -0,0 +1,8 @@
+// This function is an action creator, it needs to return an action
+// an object with a type property and then something more about the action as payload.
+export function UpdateUserPortfolio(portfolio) {
+  return {
+    type: 'USER_PORTFOLIO',
+    payload: portfolio,
+  }
+}
