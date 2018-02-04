4a488a0e9d06ad0a769e825b5a49a16a8db75020
Neha Chaudhary <neha.chaudhary@hotmail.com>
2018-02-04 07:47:57 -0800

    Rename imported modules for readability

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import RiskToleranceLevels from './risk-tolerance/levels';
-import IdealPortfolio from './charts/ideal-portfolio';
-import UserPortfolioForm from './user-portfolio/index';
-import './App.css';
+import RiskLevels from './risk-levels/levels';
+import IdealPortfolio from './charts/ideal-chart';
+import UserPortfolio from './user-portfolio/index';
+import './app.css';
 
 class App extends Component {
   render() {
     return (
       <div>
-      <h2>BrightPlan</h2>
-        <RiskToleranceLevels />
+        <h2>BrightPlan</h2>
+        <RiskLevels />
         <IdealPortfolio />
-        <UserPortfolioForm />
+        <UserPortfolio />
       </div>
     );
   }
 }
 
-function mapStateToProps(state) {
-  return {
-    selectedLevel: state.selectedLevel
-  }
-}
-
-export default connect(mapStateToProps)(App);
+export default App;
