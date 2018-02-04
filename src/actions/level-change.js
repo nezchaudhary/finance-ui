770dfe562fec481ff7b6394bf8bd8342cc31b2f7
Neha Chaudhary <neha.chaudhary@hotmail.com>
2018-02-04 07:51:43 -0800

    Move comments out of function

--- src/actions/level-change.js
+++ src/actions/level-change.js
@@ -1,6 +1,6 @@
+// select level is an action creator, it needs to return an action
+// an object with a type property and then something more about the action as payload.
 export function ChangeLevel(level) {
-  // select level is an action creator, it needs to return an action
-  // an object with a type property and then something more about the action as payload.
   return {
     type: 'SELECTED_LEVEL',
     payload: level,
