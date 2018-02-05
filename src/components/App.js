e62b5fa5fbdc7c8a54c5fd1cf2ab5a1c58a07d4a
Neha Chaudhary <neha.chaudhary@hotmail.com>
2018-02-04 18:25:03 -0800

    Change folder name

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import RiskLevels from './risk-levels/levels';
 import IdealPortfolio from './charts/ideal-chart';
-import UserPortfolio from './user-portfolio/index';
+import UserPortfolio from './portfolio-form/index';
 import './app.css';
 
 class App extends Component {
