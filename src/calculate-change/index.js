96bb182c581f9365c74aa17bece5fc0462a17c8a
Neha Chaudhary <neha.chaudhary@hotmail.com>
2018-02-05 11:09:29 -0800

    Change filename to be more expressive and modify calculateDifference function

--- src/calculate-change/index.js
+++ src/calculate-change/index.js
@@ -2,8 +2,8 @@
 
 // Input: Ideal portfolio based on risk in percentages, current user portfolio
 // Output: Minimum value of investments that need to be moved to match ideal and current
-// Constraint: Minimum adjustments that need to be made
-// Edge Cases: Floating points, rounded figures might not add up.
+// Constraint: Minimum adjustments that need to be made, keep complexity at lowest
+// Edge Cases: Floating points, rounded figures might not add up, portfolio is empty
 
 Strategy:
 // Calculate user portfolio size by adding all investment values
@@ -28,14 +28,16 @@ Transformation:
   shrink = []; increase = [];
 */
 
-// Dummy data
-const ideal = { Cash: 20, Bonds: 5, 'Mutual Funds': 25, Gold: 15, Stocks: 35 };
-const userPortfolio1 = { Cash: 70, Bonds: 46, 'Mutual Funds': 67, Gold: 87, Stocks: 32 };
-const userPortfolio2 = { Cash: 5000, Bonds: 3888, 'Mutual Funds': 4638, Gold: 4342, Stocks: 75833 };
-const userIdeal1 = { Cash: 60, Bonds: 15, 'Mutual Funds': 76, Gold: 45, Stocks: 106 };
-const userIdeal2 = { Cash: 18740, Bonds: 4685, 'Mutual Funds': 23425, Gold: 14055, Stocks: 32795 };
-const types = Object.keys(ideal); // Array of different types of investments
-
+// Dummy data for testing
+const ideal1 = { Cash: 20, Bonds: 5, 'Mutual Funds': 25, Gold: 15, Stocks: 35 };
+const types = Object.keys(ideal1); // Array of different types of investments
+// const ideal2 = { Cash: 35, Bonds: 0, 'Mutual Funds': 15, Gold: 30, Stocks: 20 };
+// const userPortfolio1 = { Cash: 70, Bonds: 46, 'Mutual Funds': 67, Gold: 87, Stocks: 32 };
+// const userPortfolio2 = { Cash: 5000, Bonds: 3888, 'Mutual Funds': 4638, Gold: 4342, Stocks: 75833 };
+// const userPortfolio3 = { Cash: 2, Bonds: 6, 'Mutual Funds': 0, Gold: 4, Stocks: 9 };
+// const userPortfolio4 = { Cash: 0, Bonds: 0, 'Mutual Funds': 0, Gold: 0, Stocks: 0 };
+// const userIdeal1 = { Cash: 60, Bonds: 15, 'Mutual Funds': 76, Gold: 45, Stocks: 106 };
+// const userIdeal2 = { Cash: 18740, Bonds: 4685, 'Mutual Funds': 23425, Gold: 14055, Stocks: 32795 };
 
 // this function returns the total size of user's portfolio
 const calculateSumOfAllInvestments = (portfolio) => {
@@ -50,14 +52,14 @@ const calculateSumOfAllInvestments = (portfolio) => {
 const calculateIdealUserPortfolio = (userPortfolio, riskPortfolio) => {
   const idealPortfolioPercentages = Object.values(riskPortfolio);
   const userPortfolioSize = calculateSumOfAllInvestments(userPortfolio);
-  let valueUntilNow = 0;
+  let sumOfValues = 0;
   return idealPortfolioPercentages.map((percent, i) =>  {
     let value;
     if (i !== types.length - 1) {
       value = Math.round((percent / 100) * userPortfolioSize);
-      valueUntilNow += value;
+      sumOfValues += value;
     } else {
-      value = userPortfolioSize - valueUntilNow;
+      value = userPortfolioSize - sumOfValues;
     }
     return value;
   });
@@ -66,24 +68,40 @@ const calculateIdealUserPortfolio = (userPortfolio, riskPortfolio) => {
 // this function calculates the difference in portfolios
 const calculateDifferenceInDollars = (userPortfolio, riskPortfolio) => {
   const userIdealPortfolio = calculateIdealUserPortfolio(userPortfolio, riskPortfolio);
-  console.log('user ideal', userIdealPortfolio);
+  // console.log('user ideal', userIdealPortfolio);
   const userInvestments = Object.values(userPortfolio);
-  const difference = userInvestments.map((current, i) => (
-    { name: types[i], value: userIdealPortfolio[i] - current}
-  ));
-  return difference;
+  const differences = userInvestments.reduce((result, current, i) => {
+    const difference = userIdealPortfolio[i] - current;
+    if (difference !== 0) {
+      result.push({ name: types[i], value: difference })
+    }
+    return result;
+  }, []);
+  // console.log('differences', differences.slice());
+  return differences;
 };
 
 const sortDifferencesByIncreaseAndDecrease = (differences) => {
   // Divide differences into investments that need to increase and decrease
-  const sortedDifferences = differences.reduce((result, investment) => {
-    if (investment.value < 0) result.decrease.push(investment);
-    if (investment.value > 0) result.increase.push(investment);
-    return result;
-  }, { decrease: [], increase: [] });
-  // Sorting decrease and increase by their max value
-  const decrease = sortedDifferences.decrease.sort((a, b) => b - a);
-  const increase = sortedDifferences.increase.sort((a, b) => a - b);
+  const sorted = differences.sort((a, b) => a.value - b.value);
+  let decreaseEnd;
+  let increaseStart;
+  
+  for (let i = 0; i < sorted.length; i++) {
+    if (decreaseEnd && increaseStart) {
+      break;
+    }
+
+    if (sorted[i].value === 0) {
+      decreaseEnd = i;
+    } else if (sorted[i].value > 0) {
+      increaseStart = i;
+      if (!decreaseEnd) decreaseEnd = i;
+    }
+  }
+
+  const decrease = sorted.slice(0, decreaseEnd).reverse();
+  const increase = sorted.slice(increaseStart);
   return { decrease, increase };
 }
 
@@ -95,11 +113,13 @@ const shiftInvestments = (user, ideal) => {
   
   // Run a while loop until values have been adjusted
   while(decrease.length) {
+    // console.log('increase', increase.slice());
+    // console.log('decrease', decrease.slice());
     let maxDecrease = decrease[decrease.length - 1];
     let maxIncrease = increase[increase.length - 1];
     if (maxIncrease.value - Math.abs(maxDecrease.value) > 0) { // max increase is larger than max decrease
       investmentsToMove.push(`Move $${Math.abs(maxDecrease.value)} from ${maxDecrease.name} to ${maxIncrease.name}`);
-      increase[increase.length - 1].difference -= Math.abs(maxDecrease.value);
+      increase[increase.length - 1].value -= Math.abs(maxDecrease.value);
       decrease.pop();
     } else if (maxIncrease.value - Math.abs(maxDecrease.value) < 0) { // max decrease is larger than max increase
       investmentsToMove.push(`Move $${maxIncrease.value} from ${maxDecrease.name} to ${maxIncrease.name}`);
@@ -114,5 +134,9 @@ const shiftInvestments = (user, ideal) => {
   return investmentsToMove;
 };
 
-console.log(shiftInvestments(userPortfolio1, ideal));
-console.log(shiftInvestments(userPortfolio2, ideal));
+// console.log(shiftInvestments(userPortfolio1, ideal1));
+// console.log(shiftInvestments(userPortfolio2, ideal1));
+// console.log(shiftInvestments(userPortfolio3, ideal2));
+// console.log(shiftInvestments(userPortfolio4, ideal2));
+
+export { shiftInvestments, calculateSumOfAllInvestments, calculateIdealUserPortfolio };
