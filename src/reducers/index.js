cda95c340f5b0a6bf147e5278a1961fcffbcb8c2
Neha Chaudhary <neha.chaudhary@hotmail.com>
2018-02-04 18:21:19 -0800

    Add reducer and application state for default investments

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import LevelsReducer from './levels-reducer';
 import SelectedLevelReducer from './change-level-reducer';
 import UserPortfolioReducer from './user-portfolio-reducer';
+import InvestmentTypesReducer from './investment-reducer';
 
 const rootReducer = combineReducers({
   levels: LevelsReducer,
   selectedLevel: SelectedLevelReducer,
-  userPortfolio: UserPortfolioReducer
+  userPortfolio: UserPortfolioReducer,
+  types: InvestmentTypesReducer
 });
 
 export default rootReducer;
---
+++ src/reducers/investment-reducer.js
@@ -0,0 +1,9 @@
+export default function () {
+  return [
+    { name: 'Cash', color: 'red' },
+    { name: 'Bonds', color: 'blue' },
+    { name: 'Mutual Funds', color: 'green' },
+    { name: 'Gold', color: 'orange' },
+    { name: 'Stocks', color: 'yellow' }
+  ];
+};
