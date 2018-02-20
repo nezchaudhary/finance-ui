aa423347a4e9ba2b9a86d34cf713a4bf643ac044
Neha Chaudhary <neha.chaudhary@hotmail.com>
2018-02-20 10:44:51 -0800

    Organise import statements

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-
-import './index.css';
-import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+
 import reducers from './reducers';
+import App from './components/App';
+import './index.css';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
